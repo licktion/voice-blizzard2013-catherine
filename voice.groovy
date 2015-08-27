b7bbb3200d84e1b13905796e27e9e3127e111f9f
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-08-27 11:40:22 +0200

    use gradle voicebuilding plugin
    this must be manually cloned from https://github.com/psibre/gradle-marytts-voicebuilding-plugin/tree/rebuild and installed to mavenLocal

---
+++ voice.groovy
@@ -0,0 +1,16 @@
+voice {
+    name = 'blizzard2013-catherine'
+    locale {
+        language = 'en'
+        country = 'US'
+    }
+    gender = 'female'
+    type = 'unit selection'
+    description = 'TBA'
+    license {
+        name = 'foo'
+        shortName = 'foo'
+        url = 'bar'
+    }
+    samplingRate = 16000
+}
